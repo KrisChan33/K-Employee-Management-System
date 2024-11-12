0ce466407bfcadd5c5fb295456178ca0a2a3f347
Kristian Jay Tadeo <Kristianjaytadeo11@gmail.com>
2024-11-12 15:07:01 +0800

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,11 +3,9 @@
 namespace App\Providers\Filament;
 
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
-use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Filament\Navigation\MenuItem;
 use Filament\Navigation\MenuItem;
 use Filament\Pages;
 use Filament\Panel;
