95658b5bf5243e6128e657ede87c0a2af3608253
Kristian Jay Tadeo <Kristianjaytadeo11@gmail.com>
2024-12-12 22:35:57 +0800

    Remove /app directory from the repository and add to .gitignore

--- app/Console/clear_all_caches.sh
+++
@@ -1,9 +0,0 @@
-php artisan cache:clear
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
-php artisan event:clear
-php artisan clear-compiled
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
